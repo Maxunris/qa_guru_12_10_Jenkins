c01237baaaf714fec3000c3737ca695d7322a880
Maxim Rusin <Maxunris@gmail.com>
2024-03-07 09:26:33 +0300

    add new func

--- demoqa_tests/pages/registration_page.py
+++ demoqa_tests/pages/registration_page.py
@@ -6,8 +6,8 @@ class RegistrationPage:
 
     @allure.step('Открытие формы регистрации')
     def open(self):
-        browser.open("https://demoqa.com/automation-practice-form")
-        # browser.element(".fc-button-label").click()
+        browser.open("/automation-practice-form")
+        browser.element(".fc-button-label").click()
 
     @allure.step('Заполнение имени')
     def fill_first_name(self, first_name):
